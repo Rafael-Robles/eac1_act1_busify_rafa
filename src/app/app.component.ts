bca50e1593df4019e7bb2fa596158d476eeb05ec
Rafael Robles Perez <rafaelrobles.alum@insestatut.cat>
2026-03-02 17:44:14 +0100

    Modificacio del html, scss i afegim confirmació al console log del bon funcionament

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ioc-angular-busify-rafa';
+  constructor()
+  {
+    console.log("Busify d'ha inicialitzat correctament");
+  }
 }
